e647cfe87f3431cb54e1fc55ff1b3fb1ca27c7c1
Michael Jones <michael.jones90@me.com>
2026-04-09 17:13:39 +0100

    Initial Commit

---
+++ HackingWithSwift-SumThing/HackingWithSwift_SumThingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwift_SumThingApp.swift
+//  HackingWithSwift-SumThing
+//
+//  Created by Michael Jones on 09/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwift_SumThingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
