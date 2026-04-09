2603210d5ed58d464d53a57fc429cff8438542a2
Michael Jones <michael.jones90@me.com>
2026-04-09 21:06:17 +0100

    - Logic to include the target numbers around the Grid.

--- HackingWithSwift-SumThing/Views/ContentView.swift
+++ HackingWithSwift-SumThing/Views/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
                 Grid(horizontalSpacing: 2, verticalSpacing: 2) {
                     ForEach(0..<board.exampleCells.count, id: \.self) { row in
                         GridRow {
+                            let exampleRow = board.exampleCells[row]
                             let userRow = board.userCells[row]
                             
                             ForEach(0..<userRow.count, id: \.self) { col in
@@ -28,15 +29,50 @@ struct ContentView: View {
                                     board.selectedCol = col
                                 }
                             }
+                            
+                            let exampleSum = sum(forRow: exampleRow)
+                            let userSum = sum(forRow: userRow)
+                            
+                            SumView(number: exampleSum)
+                                .foregroundStyle(exampleSum == userSum ? .black : .red)
                         }
                     }
+                    GridRow {
+                        ForEach(0..<board.exampleCells[0].count, id: \.self) { col in
+                            let exampleSum = sum(forCol: col, in: board.exampleCells)
+                            let userSum = sum(forCol: col, in: board.userCells)
+                            
+                            SumView(number: exampleSum)
+                                .foregroundStyle(exampleSum == userSum ? .black : .red)
+                        }
+                    }
+                    
                 }
                 .padding(.horizontal)
                 
                 Spacer()
+                
+                HStack {
+                    ForEach(1..<10) { i in
+                        Button(String(i)) {
+                            board.enter(i)
+                        }
+                        .frame(maxWidth: .infinity)
+                        .font(.largeTitle)
+                    }
+                }
+                .padding()
             }
             .navigationTitle(Text("SumThing"))
         }
+    }
+    
+    func sum(forRow row: [Int]) -> Int {
+        row.reduce(0, +) // Loop over the input array, starting at 0, adding them all together. Then send it back.
+    }
+    
+    func sum(forCol col: Int, in cells: [[Int]]) -> Int {
+        cells.reduce(0) { $0 + $1[col] }
     }
 }
 
